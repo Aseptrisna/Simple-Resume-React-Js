4888a3d232f09361dec5a9a9112396955d61a345
Aseptrisna <Aseptrisna1995@gmail.com>
2022-05-27 20:16:39 +0700

    done v1

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
+import Team from "./Components/Team";
+import TeamLead from "./Components/Lead";
+import Teamdesigner from "./Components/Designer"
+import Mitra from "./Components/Mitra"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {}
+    };
+
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: "./resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function(data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main} />
+        {/* <About data={this.state.resumeData.main} /> */}
+        {/* <Resume data={this.state.resumeData.resume} /> */}
+       
+        <TeamLead data={this.state.resumeData.lead} />
+        <Team data={this.state.resumeData.team} />
+        <Teamdesigner data={this.state.resumeData.designer} />
+        {/* <Mitra data={this.state.resumeData.mitra} /> */}
+         <Portfolio data={this.state.resumeData.portfolio} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
+      </div>
+    );
+  }
+}
+
+export default App;
